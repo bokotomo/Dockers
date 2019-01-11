d476598be298c7f4f9ee79fad9aed10897819e35
bokotomo <bokotomo@me.com>
2019-01-11 18:59:56 +0900

    feat: vue-cli3

--- tomo-slack-post/app.py
+++ tomo-slack-post/app.py
@@ -10,7 +10,7 @@ redis = Redis(host='redis', port=6379)
 def index():
     val = request.args.get("msg", "Not defined")
     count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n<br>{}'.format(count, val)
+    return 'Hello World! I have been seen {} times!\n<br>{}'.format(count, val)
 
 @app.route('/api/v1/send', methods=['POST'])
 def send_slack():
